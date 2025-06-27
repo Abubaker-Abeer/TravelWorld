253923bbd60cf41a97f0ec4d720d43db4abd5e28
abeer-abubaker <abeerabubaker2001@gmail.com>
2025-06-27 18:37:48 +0300

    Created About Us page with company intro, services section, and newsletter

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -4,11 +4,14 @@ import Home from "../pages/Home";
 import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
 import ProtectedRoute from "../router/ProtectedRoute";
+import About from "../pages/About";
 
 function Routers() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home /> }/>
